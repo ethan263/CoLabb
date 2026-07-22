ce0208f6a43d88a4410e939808f339060c646e37
Ethan Dietrich <ethandietrich263@gmail.com>
2026-07-22 22:37:10 +0200

    updated gsap config

--- lib/gsap.ts
+++ lib/gsap.ts
@@ -1,1 +1,12 @@
-npm install -D tailwindcss postcss autoprefixer
+"use client";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+// Register plugins safely on the browser client side
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger, useGSAP);
+}
+
+export { gsap, ScrollTrigger, useGSAP };
